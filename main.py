66d393367b2388ffe20a60eff0f1cd220581e55b
huushuur <160753738+huushuur@users.noreply.github.com>
2024-06-27 23:13:54 +0000

    #4.9 Recap + Code Challenge

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from requests import get
 
 websites = (
   "google.com",
-  "airbnb.com",
-  "https://twitter.com",
-  "facebook.com",
-  "https://tiktok.com"
+  "https://httpstat.us/502",
+  "https://httpstat.us/404",
+  "https://httpstat.us/300",
+  "https://httpstat.us/200",
+  "https://httpstat.us/101"
 )
 
 results = {}
@@ -14,9 +15,15 @@ for website in websites:
   if not website.startswith("https://"):
     website = f"https://{website}"
   response = get(website)
-  if response.status_code == 200:
+  if response.status_code >= 500:
+    results[website] = "SERVER ERROR"
+  elif response.status_code >= 400:
+    results[website] = "CLIENT ERROR"
+  elif response.status_code >= 300:
+    results[website] = "REDIRECT"
+  elif response.status_code >= 200:
     results[website] = "OK"
-  else:
-    results[website] = "FAILED"
+  elif response.status_code >= 100:
+    results[website] = "INFORMATION"
 
 print(results)

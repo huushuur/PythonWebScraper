3511fea101ed4f725938098599375b383461d519
huushuur <160753738+huushuur@users.noreply.github.com>
2024-07-02 13:33:28 +0000

    #6.5 Pagination

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
+all_jobs = []
 
-response = requests.get(url)
 
-soup = BeautifulSoup(
-    response.content,
-    "html.parser",
-)
+def scrape_page(url):
+    response = requests.get(url)
 
-jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+    soup = BeautifulSoup(
+        response.content,
+        "html.parser",
+    )
 
-for job in jobs:
-  title = job.find("span", class_="title").text
-  company, position, region = job.find_all("span", class_="company")[0:3]
-  company = company.text
-  position = position.text
-  region = region.text
-  print(title, company, position, region, "---------\n")
+    jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+
+    for job in jobs:
+        title = job.find("span", class_="title").text
+        job_info = job.find_all("span", class_="company")
+        company = job_info[0].text if len(job_info) > 0 else ""
+        position = job_info[1].text if len(job_info) > 1 else ""
+        region = job_info[2].text if len(job_info) > 2 else ""
+        try:
+            url = job.find("div",
+                           class_="tooltip--flag-logo").next_sibling["href"]
+        except KeyError:
+            url = "You need log-in"
+        job_data = {
+            "title": title,
+            "company": company,
+            "position": position,
+            "region": region,
+            "url": f"https://weworkremotely.com{url}"
+        }
+        all_jobs.append(job_data)
+
+
+def get_pages(url):
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    return len(
+        soup.find("div", class_="pagination").find_all("span", class_="page"))
+
+
+total_pages = get_pages(
+    "https://weworkremotely.com/remote-full-time-jobs?page=1")
+
+for x in range(total_pages):
+    url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
+    scrape_page(url)
+print(all_jobs)

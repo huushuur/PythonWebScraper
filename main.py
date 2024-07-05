1263b9053454282db58fbed6b17ddc1c621c2102
huushuur <160753738+huushuur@users.noreply.github.com>
2024-07-05 06:14:53 +0000

    remoteok CC

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 class JobInfo:
+
     def __init__(self, url, position, company, location, salary):
         self.url = f"https://remoteok.com{url}"
         self.position = position
@@ -11,17 +13,19 @@ class JobInfo:
 
     def __str__(self):
         return f"Url: {self.url} \n Position: {self.position} \n Company: {self.company} \n Location: {self.location} \n Salary: {self.salary}"
-        
+
+
 class JobScraper:
+
     def __init__(self, keyword):
         self.url = f"https://remoteok.com/remote-{keyword}-jobs"
         self.headers = {
             "User-Agent":
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
         }
-        self.jobs = []
+        self.jobinfos = []
 
-    def print_jobs(self):
+    def print_jobinfos(self):
         response = requests.get(self.url, headers=self.headers)
         soup = BeautifulSoup(response.content, "html.parser")
         job_elements = soup.find_all("td", class_="company_and_position")[1:]
@@ -31,27 +35,21 @@ class JobScraper:
             position = job_element.find("h2", itemprop="title").text.strip()
             company = job_element.find("h3", itemprop="name").text.strip()
             class_location = job_element.find_all("div", class_="location")
-            location = class_location[0].text.strip() if len(class_location) > 0 else ""
-            salary = class_location[1].text.strip() if len(class_location) > 1 else ""
-            job = JobInfo(url, position, company, location, salary)
-            self.jobs.append(job)
+            location = class_location[0].text.strip() if len(
+                class_location) > 0 else ""
+            salary = class_location[1].text.strip() if len(
+                class_location) > 1 else ""
+            jobinfo = JobInfo(url, position, company, location, salary)
+            self.jobinfos.append(jobinfo)
 
         print("-" * 20 + f"{keyword}" + "-" * 20)
-        for job in self.jobs:
+        for job in self.jobinfos:
             print(job)
         print("\n")
-    
+
 
 keywords = ["flutter", "python", "golang", "c-sharp", "javascript"]
 
 for keyword in keywords:
     scraper = JobScraper(keyword)
-    scraper.print_jobs()
-
-
-
-
-
-
-
-
+    scraper.print_jobinfos()
